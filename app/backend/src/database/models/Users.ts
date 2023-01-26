88b867646436bfc378aa8709689ca0f31bed5d74
Windson Macedo <windsondev@hotmail.com>
2023-01-26 15:44:19 -0300

    build: model users e migration users

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,36 @@
+import { Model, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+export default class User extends Model {
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  // ... Campos
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
